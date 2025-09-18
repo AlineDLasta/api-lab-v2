a9b1eb91e344550ffc03e8b695eb5bdf3a8140e9
Aline Dias Lasta <diaslasta@gmail.com>
2025-09-18 20:19:32 -0300

    Adding mapping functionality

--- src/main/java/com/example/api_lab_v2/Book.java
+++ src/main/java/com/example/api_lab_v2/Book.java
@@ -2,6 +2,16 @@ package com.example.api_lab_v2;
 
 public class Book {
     private String titulo;
+    public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public Book setId(int id) {
+        this.id = id;
+        return this;
+    }
 
     public Book(){}
 
--- src/main/java/com/example/api_lab_v2/BookController.java
+++ src/main/java/com/example/api_lab_v2/BookController.java
@@ -17,4 +17,18 @@ public class BookController {
         livro.setTitulo("Aqua Tofana");
         return livro;
     }
+
+    @GetMapping("/book/{id}")
+    public Book buscarLivroPorId(@PathVariable int id){
+        Book livro = new Book();
+        livro.setTitulo("Livro com id: " + id);
+        return livro;
+    }
+
+    @GetMapping("/book/search")
+    public Book buscarLivroPorTitulo(@RequestParam String titulo){
+        Book livro = new Book();
+        livro.setTitulo("Você pesquisou pelo título: " + titulo);
+        return livro;
+    }
 }
